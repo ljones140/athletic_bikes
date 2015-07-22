a38028dd887dd2c914d766164232c129a6f37046
Leon Wee <leonwee27@gmail.com>
2015-07-22 23:41:08 +0700

    Added a new Rspec Test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,5 @@
 class DockingStation
   def release_bike
+    fail "No Bikes Available"
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
+  describe '#release_bike' do
+    it 'should have an empty docking station by default' do
+      expect {subject.release_bike}.to raise_error "No Bikes Available"
+    end
+  end
+
+
   it 'releases bike' do
     expect(subject).to respond_to :release_bike
   end

fde7c27e25ef7ef0a28a2593a3596785369ff059
lewis <ljones140@gmail.com>
2015-07-23 14:23:07 +0100

    get working bike from docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+
   def release_bike
     Bike.new
+  end
+
+  def empty?
+    true
+  end
+
+  def dock(bike)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,14 +6,28 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+  it "responds to empty" do
+    expect(subject).to respond_to :empty?
+  end
+
+  it "responds to empty" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
   it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it "is empty by default" do
+    expect(subject).to be_empty
+  end
+
 
   describe "#release_bike" do
     it "releases a Bike" do
       expect(subject.release_bike).to be_a Bike
     end
   end
+
 end

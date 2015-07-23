94bbf9ce8e87b269a30040ccf9b22223e6026a62
Leon Wee <leonwee27@gmail.com>
2015-07-23 18:56:42 +0700

    Done a bit of Challenge 3

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,17 @@
 class Bike
+
+  attr_reader :broken
+
+  def initialize
+    @broken = false
+  end
+
   def working?
     true
   end
+
+  def report_broken
+    @broken = true
+  end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,16 @@ describe Bike do
   it "responds to working" do
     expect(subject).to respond_to :working?
   end
+
+
+  # it "report bike is broken" do
+  #   expect(subject).to respond_to :report_broken
+  # end
+
+  it "returns true" do
+    bike = Bike.new
+    bike.report_broken
+    expect(bike.broken).to be true
+  end
+
 end

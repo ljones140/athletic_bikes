0113a1155b26f63a321f8329d2ab70802305539b
Leon Wee <leonwee27@gmail.com>
2015-07-23 12:18:38 +0700

    Done first part of user story

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,11 +6,14 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+  it "releases working bikes" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
   describe "#release_bike" do
     it "releases a Bike" do
       expect(subject.release_bike).to be_a Bike
     end
   end
-
-
 end

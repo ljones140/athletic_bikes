a3276851d7c243d6847a0109ddc08a2050f3ea45
lewis <ljones140@gmail.com>
2015-07-23 16:48:50 +0100

    Errors when capacity over 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -24,10 +24,16 @@ describe DockingStation do
     expect(subject).to be_empty
   end
 
-
   describe "#release_bike" do
     it "raises an error when there are no bikes" do
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe "#dock" do
+    it "raises error when full" do
+      20.times{subject.dock(Bike.new)}
+      expect {subject.dock(Bike.new)}.to raise_error 'Capacity full'
     end
   end
 
